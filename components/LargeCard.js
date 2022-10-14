445e88d882016195caf6a65ea304dd88a0871d30
AlinOrobet <alin.orobet@student.usv.ro^C>
2022-10-15 01:29:27 +0300

    v1.0

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -3,7 +3,7 @@ function LargeCard({img, title, description, buttonText}) {
   return (
     <section className="relative py-16 cursor-pointer">
       <div className="relative h-96 min-w-[300px]">
-        <Image className="rounded-2xl" src={img} layout="fill" objectFit="cover" />
+        <Image className="rounded-2xl" src={img} layout="fill" objectFit="cover" alt="card" />
       </div>
       <div className="absolute top-32 left-12">
         <h3 className="text-4xl mb-3 w-64">{title}</h3>
